5fa58594237d9268a664e34d2aad715147f685d1
Sbonelo01 <sbonelomkhize15@gmail.com>
2020-03-06 15:57:50 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,107 @@
+"use strict";
+
+const {
+    Client
+} = require("pg");
+
+const client = new Client({
+    user: "user",
+    host: "localhost",
+    password: "pass",
+    port: 5432,
+    database: "db"
+});
+
+const addNewVisitor = async(visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments) => {
+    return new Promise(async(resolve, reject) => {
+        await client.query(`INSERT INTO VISITORS(visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments) VALUES($1,$2,$3,$4,$5,$6) RETURNING *`, [vName, vAge, dateOfVisit, timeOfVisit, assistantName, comments],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results.rows[0]);
+            });
+    })
+
+}
+
+const listAllVisitors = async() => {
+    return new Promise(async(resolve, reject) => {
+        await client.query(
+            `SELECT * FROM VISITORS`,
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results.rows);
+            }
+        );
+    })
+};
+
+const viewVisitor = async(id) => {
+    return new Promise(async(resolve, reject) => {
+        await client.query(
+            `SELECT * FROM visitors WHERE id = $1`, [id],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results.rows[0]);
+            }
+        );
+    })
+};
+
+const deleteVisitors = async() => {
+    return new Promise(async(resolve, reject) => {
+        await client.query(
+            `DELETE FROM visitors `, (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log('All rows deleted!')
+                resolve(results.rows);
+            }
+        );
+    })
+};
+
+const deleteVisitor = async(id) => {
+    return new Promise(async(resolve, reject) => {
+        await client.query(
+            `DELETE FROM visitors WHERE id = $1`, [id],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                console.log('deleted!!!')
+                resolve(results);
+            }
+        );
+    })
+};
+
+const updateVisitor = async(id, visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments) => {
+    return new Promise(async(resolve, reject) => {
+        await client.query(
+            `UPDATE visitors SET visitorName = $2, visitorAge = $3,dateOfVisit = $4,timeOfVisit=$5,assistantName = $6,comments =$7 WHERE id = $1 RETURNING *`, [vid, vName, vAge, dateOfVisit, timeOfVisit, assistantName, comments],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results.rows[0]);
+            }
+        );
+    })
+};
+
+
+module.exports = {
+    addNewVisitor,
+    listAllVisitors,
+    viewVisitor,
+    deleteVisitors,
+    deleteVisitor,
+    updateVisitor
+}
---
+++ src/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const {
+    addNewVisitor,
+    listAllVisitors,
+    deleteVisitor,
+    deleteVisitors,
+    viewVisitor,
+    updateVisitor
+} = require('./app');
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.post('/addNewVisitor', async(req, res) => {
+    const name = req.body.visitorName
+    const age = req.body.visitorAge
+    const dateOfVisit = req.body.dateOfVisit
+    const timeOfVisit = req.body.timeOfVisit
+    const nameOfAssistant = req.body.nameOfAssistant
+    const comments = req.body.comments
+    const visitor = await addNewVisitor(visitorName, visitorAge, dateOfVisit, timeOfVisit, nameOfAssistant, comments)
+    res.send(JSON.stringify(visitor));
+    res.end();
+})
+
+app.delete('/deleteVisitor/:id', async(req, res) => {
+    const visitor = await deleteVisitor(req.params.id);
+    res.send(JSON.stringify(visitor));
+    res.end();
+})
+
+app.delete('/deleteVisitors', async(req, res) => {
+    const visitor = await deleteVisitors();
+    res.send(JSON.stringify(visitor));
+    res.end();
+})
+
+app.get('/viewVisitors', async(req, res) => {
+    const visitor = await listAllVisitors();
+    res.send(JSON.stringify(visitor));
+    res.end();
+})
+
+app.get('/viewVisitor/:id', async(req, res) => {
+    const visitor = await viewVisitor(req.params.id);
+    res.send(JSON.stringify(visitor));
+    res.end();
+
+})
+
+app.put('/updateVisitor/:id', async(req, res) => {
+    const id = req.params.id
+    const name = req.body.visitorName
+    const age = req.body.visitorAge
+    const dateOfVisit = req.body.dateOfVisit
+    const timeOfVisit = req.body.timeOfVisit
+    const nameOfAssistant = req.body.nameOfAssistant
+    const comments = req.body.comments
+    const visitor = await updateVisitor(id, visitorName, visitorAge, dateOfVisit, timeOfVisit, nameOfAssistant, comments)
+    res.send(JSON.stringify(visitor));
+    res.end();
+})
+
+const server = app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+});
