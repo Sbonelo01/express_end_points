b0426ea18a1ebdb237f65b5361174913cd9df72a
Sbonelo <37469171+Sbonelo01@users.noreply.github.com>
2020-04-20 13:57:46 +0200

    Update app.js
    fixed conn. issues

--- src/app.js
+++ src/app.js
@@ -14,85 +14,105 @@ const client = new Client({
     database: process.env.PGDATABASE
 });
 
-const addNewVisitor = async(visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments) => {
-    return new Promise(async(resolve, reject) => {
-        await client.query(`INSERT INTO VISITORS(visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments) VALUES($1,$2,$3,$4,$5,$6) RETURNING *`, [vName, vAge, dateOfVisit, timeOfVisit, assistantName, comments],
-            (err, results) => {
-                if (err) {
-                    reject(err);
-                }
-                resolve(results.rows[0]);
-            });
-    })
+client.connect();
 
+const createTable = async() => {
+    return new Promise(async(request, response) => {
+        let sql = await client.query(`CREATE TABLE IF NOT EXISTS VISITORS(
+                id SERIAL PRIMARY KEY,
+                visitorName varchar(255) NOT NULL,
+                assistant varchar(255) NOT NULL,
+                visitorAge int NOT NULL,
+                dateOfVisit date NOT NULL,
+                timeOfVisit time NOT NULL,
+                comments text NOT NULL
+                )`,
+            (error, results) => {
+                if (error) {
+                    throw error;
+                }
+                //console.log(sql
+            }
+        )
+    })
 }
 
-const listAllVisitors = async() => {
-    return new Promise(async(resolve, reject) => {
-        await client.query(
-            `SELECT * FROM VISITORS`,
-            (err, results) => {
-                if (err) {
-                    reject(err);
+const addNewVisitor = async(visitorName, assistant, visitorAge, dateOfVisit, timeOfVisit, comments) => {
+    return new Promise(async(request, response) => {
+        let results = await client.query(`INSERT INTO VISITORS(visitorName, assistant, visitorAge, dateOfVisit, timeOfVisit, comments) VALUES($1,$2,$3,$4,$5,$6) RETURNING *`, [visitorName, assistant, visitorAge, dateOfVisit, timeOfVisit, comments],
+            (error, results) => {
+                if (error) {
+                    throw error;
                 }
-                resolve(results.rows);
-            }
-        );
+                console.log(results.rows);
+            });
     })
 };
 
+const listAllVisitors = async(request, response) => {
+    let results = await client.query(
+        `SELECT * FROM visitors ORDER BY id ASC`,
+        (error, results) => {
+            if (error) {
+                throw error;
+            }
+            console.log(results.rows)
+        }
+    );
+};
+
 const viewVisitor = async(id) => {
-    return new Promise(async(resolve, reject) => {
-        await client.query(
+    return new Promise(async(request, response) => {
+        let results = await client.query(
             `SELECT * FROM visitors WHERE id = $1`, [id],
             (err, results) => {
                 if (err) {
-                    reject(err);
+                    throw err;
                 }
-                resolve(results.rows[0]);
+                request(results.rows[0]);
             }
         );
     })
 };
 
 const deleteVisitors = async() => {
-    return new Promise(async(resolve, reject) => {
-        await client.query(
+    return new Promise(async(request, response) => {
+        let results = await client.query(
             `DELETE FROM visitors `, (err, results) => {
                 if (err) {
-                    reject(err);
+                    throw err;
                 }
                 console.log('All rows deleted!')
-                resolve(results.rows);
+                request(results.rows);
             }
         );
     })
 };
 
 const deleteVisitor = async(id) => {
-    return new Promise(async(resolve, reject) => {
-        await client.query(
+    return new Promise(async(request, response) => {
+        let results = await client.query(
             `DELETE FROM visitors WHERE id = $1`, [id],
-            (err, results) => {
-                if (err) {
-                    reject(err);
+            (error, results) => {
+                if (error) {
+                    throw error;
                 }
+                request(results.rows);
                 console.log('deleted!!!')
-                resolve(results);
             }
         );
     })
 };
 
 const updateVisitor = async(id, visitorName, visitorAge, dateOfVisit, timeOfVisit, assistantName, comments) => {
-    return new Promise(async(resolve, reject) => {
-        await client.query(
+    return new Promise(async(request, response) => {
+        let results = await client.query(
             `UPDATE visitors SET visitorName = $2, visitorAge = $3,dateOfVisit = $4,timeOfVisit=$5,assistantName = $6,comments =$7 WHERE id = $1 RETURNING *`, [vid, vName, vAge, dateOfVisit, timeOfVisit, assistantName, comments],
             (err, results) => {
                 if (err) {
-                    reject(err);
+                    throw err;
                 }
-                resolve(results.rows[0]);
+                request(results.rows[0]);
             }
         );
     })
